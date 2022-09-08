85a266b2c7354dd2988404729267d96f31a19277
TJS Capita <trevor.smith@capita.com>
2022-09-08 11:34:45 +0100

    Initial commit

---
+++ src/UsingForLoops.java
@@ -0,0 +1,7 @@
+public class UsingForLoops {
+    public static void main(String[] args) {
+       for (int i = 10; i <= 10; i++) {
+            System.out.println("Fred " + i);
+        }
+    }
+}
---
+++ src/UsingWhileLoops.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class UsingWhileLoops {
+    public static void main(String[] args) {
+
+        // While loop using a menu!
+//        System.out.println("---  Menu  ---");
+//        System.out.println("1. Print Hello");
+//        System.out.println("2. Print Good-bye");
+//        System.out.println("x. Exit menu");
+//        String answer = "";
+//        Scanner reader = new Scanner(System.in);
+//        while (!answer.equalsIgnoreCase("x")){
+//            answer = reader.nextLine();
+//            if (answer.equals("1")) {
+//                System.out.println("Hello");
+//            } else if (answer.equals("2")) {
+//                System.out.println("Good-bye");
+//            } else if (answer.equals("x")) {
+//                System.out.println("Exit");
+//            } else {
+//                System.out.println("Entry not correct!");
+//            }
+//        }
+
+        // While Loop using Login Script!!!
+//        String username = "admin";
+//        String password = "password!";
+//        String enteredUsername = "";
+//        String enteredPassword = "";
+
+        // I need to get the user to enter a username and password
+        // if they don't match the above then access is denied
+        // if they do match then access is granted.
+
+//        Scanner reader = new Scanner(System.in);
+//
+//        while (enteredUsername.isEmpty()) {
+//            System.out.print("Enter username: ");
+//            enteredUsername = reader.nextLine().toLowerCase().trim();
+//        }
+//        System.out.print("Enter password: ");
+//        enteredPassword = reader.nextLine();
+//
+//        if (enteredUsername.equals(username) && enteredPassword.equals(password)){
+//            System.out.println("Access granted!");
+//        }
+//        else{
+//            System.out.println("Access denied!");
+//        }
+
+    }
+}
